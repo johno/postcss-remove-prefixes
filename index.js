6ab00a6f104e55acb815548441b254f34de0bccd
Rajiv Mayani <mayani@isi.edu>
2017-03-17 12:21:00 -0700

    Add support for older node versions

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 var postcss = require('postcss')
 var isVendorPrefixed = require('is-vendor-prefixed')
 
-module.exports = postcss.plugin('postcss-remove-prefixes', function (options = {}) {
+module.exports = postcss.plugin('postcss-remove-prefixes', function (options) {
+  if (!options) {
+    options = {};
+  }
+  
   let ignore = options.ignore ? Array.isArray(options.ignore) ? options.ignore : false : [];
 
   if (ignore === false) {
